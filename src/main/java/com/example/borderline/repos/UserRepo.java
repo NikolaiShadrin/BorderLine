e32cbc27f862226c0edc0ade1b8f0bdcf291907d
NikolaiShadrin <shyure@bk.ru>
2023-02-17 13:35:10 +0400

    BorderLine project

---
+++ src/main/java/com/example/borderline/repos/UserRepo.java
@@ -0,0 +1,10 @@
+package com.example.borderline.repos;
+
+import com.example.borderline.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    User findByActivationCode(String code);
+}
---
+++ target/classes/templates/greeting.ftl
@@ -0,0 +1,7 @@
+<#import "parts/common.ftl" as c>
+
+<@c.page>
+    <h5>Hello art lover</h5>
+    <div>Borderline world of high art, which you choose yourself</div>
+</@c.page>
+
